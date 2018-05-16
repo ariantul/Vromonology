8c8af0f9da4deb7bbf36cb55b686ea6ed683ef6a
12DHA270 <ariantul@gmail.cpm>
2018-05-17 00:56:36 +0600

    Arian

--- app/src/main/java/com/example/a12dha270/vromonology/UserLoginActivity.java
+++ app/src/main/java/com/example/a12dha270/vromonology/UserLoginActivity.java
@@ -2,6 +2,7 @@ package com.example.a12dha270.vromonology;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UserLoginActivity extends AppCompatActivity {
 
@@ -11,6 +12,20 @@ public class UserLoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_login);
     }
 
+    public void loginUser (View view){
 
-    
+    }
+
+    public void resetPass (View view){
+
+    }
+
+    public void createUser (View view){
+
+    }
+
+
+
+
+
 }
